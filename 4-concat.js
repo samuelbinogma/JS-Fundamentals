ef55d68888c4a5210a05b4fd0949e080b1654a72
Samuel Binogma <sbinogma41@gmail.com>
2025-09-14 21:13:46 +0000

    Fix script to print undefined is undefined for no arguments

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,4 @@
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
-if (firstArg === undefined || secondArg === undefined) {
-  console.log("No argument");
-} else {
-  console.log(`${firstArg} is ${secondArg}`);
-}
+console.log(`${firstArg} is ${secondArg}`);
