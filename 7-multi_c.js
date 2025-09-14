972d4e2462dbd24fe580c36d5b1f2c3376811358
Samuel Binogma <sbinogma41@gmail.com>
2025-09-14 21:33:00 +0000

    Fix script to print nothing when no argument is provided

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const count = Number(process.argv[2]);
 
-if (!Number.isInteger(count) || count < 0) {
+if (process.argv[2] !== undefined && (!Number.isInteger(count) || count < 0)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (process.argv[2] !== undefined) {
   for (let i = 0; i < count; i++) {
     console.log("C is fun");
   }
