d6f716d22560c0fcb21c6e3d546e8c24dbdfb43c
Samuel Binogma <sbinogma41@gmail.com>
2025-09-14 21:28:03 +0000

    add file 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const count = Number(process.argv[2]);
+
+if (!Number.isInteger(count) || count < 0) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log("C is fun");
+  }
+}
