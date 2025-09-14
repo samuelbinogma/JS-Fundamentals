1a474c8cc20e658d41ad7b0a6a72f57a0723cd3f
Samuel Binogma <sbinogma41@gmail.com>
2025-09-14 21:37:20 +0000

    Fix script to print nothing for negative integers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const count = Number(process.argv[2]);
 
-if (process.argv[2] !== undefined && (!Number.isInteger(count) || count < 0)) {
+if (process.argv[2] !== undefined && !Number.isInteger(count)) {
   console.log("Missing number of occurrences");
-} else if (process.argv[2] !== undefined) {
+} else if (process.argv[2] !== undefined && count > 0) {
   for (let i = 0; i < count; i++) {
     console.log("C is fun");
   }
